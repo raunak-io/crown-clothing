b2a7b631b8ea4f399a29a7a84513a7f7a9d04399
Raunak Srivastava <fullstackerr@gmail.com>
2021-06-09 12:44:27 +0530

    implementing routing using react router dom package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = (props) => {
+  console.log("props detector here", props);
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div >
-  <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+        {/* <HomePage /> */}
+      </Switch>
     </div>
   );
 }
